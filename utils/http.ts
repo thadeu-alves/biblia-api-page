0c536ecabc92a16c56a13017ac555960780eeb06
thadeu-alves <thadeutelecentrosg@gmail.com>
2025-08-28 13:42:47 -0300

    update to new endpoint

--- utils/http.ts
+++ utils/http.ts
@@ -1,16 +1,14 @@
 import { Book } from "@/types";
 
 export const http = {
-    baseUrl: "https://bibliaapi.vercel.app/",
+    baseUrl: "https://bibliaapi.vercel.app/livros",
     async getHome() {
         const res = await fetch(this.baseUrl);
         const data = await res.json();
         return data;
     },
     async getBook(id: number): Promise<{ data: Book }> {
-        const res = await fetch(
-            this.baseUrl + `livro/${id}`
-        );
+        const res = await fetch(this.baseUrl + `/${id}`);
         const data = await res.json();
         return data;
     },
@@ -19,7 +17,7 @@ export const http = {
         chap: number
     ): Promise<{ data: string[] }> {
         const res = await fetch(
-            this.baseUrl + `livro/${book}/${chap}`
+            this.baseUrl + `/${book}/${chap}`
         );
         const data = await res.json();
         return data;
